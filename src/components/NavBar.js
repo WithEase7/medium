e16c4e3518b58ad360fd94ecff7eb15bcdd6a969
Abhishek <abhishekbhaskar171@gmail.com>
2023-07-28 09:13:53 +0530

    Nav Bar styling added with respect to diff pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { mediumLogo } from "../utilities/SvgIcons";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../css/NavBar.css";
 
 const NavBar = () => {
   const [colorChange, setColorChange] = useState(false);
+  let location = useLocation();
   const updateNavbarStyle = () => {
     const scrollPosition = window.scrollY;
     const scrollThreshold = 350;
@@ -14,29 +15,32 @@ const NavBar = () => {
   };
   useEffect(() => {
     window.addEventListener("scroll", updateNavbarStyle);
+    return (()=> {
+      window.removeEventListener("scroll", updateNavbarStyle);
+    })
   }, []);
   return (
     <div
-      className={"navbar-background " + (colorChange ? "navbar-scrolled" : "")}
+      className={"navbar-background " + (colorChange ? "navbar-scrolled" : (location.pathname.includes("write") ? "orangeBackground" : location.pathname.includes("membership") ? "blueBackground" : ""))}
       id="navbar"
     >
-      <div className="navbar-container">
+      <div className={"navbar-container " + (location.pathname.includes("write") || location.pathname.includes("membership") ? "" : "mw1300")} id="navContainer">
         <a className="navbar-logo" href="/">
-          {mediumLogo}
+          {location.pathname.includes("membership") ? <img src="https://cdn-static-1.medium.com/sites/medium.com/membership/images/Medium-Logo-Black-RGB-1.svg" className="logoStyle"/> : mediumLogo}
         </a>
         <div className="navbar-menu-container">
           <ul className="navbar-menu">
             <li className="menu">
-              <Link to="/membership">Membership</Link>
+              <Link to="/membership" className={location.pathname.includes("membership") ? "clrw" : ""}>Membership</Link>
             </li>
             <li className="menu">
-              <Link to="/write">Write</Link>
+              <Link to="/write" className={location.pathname.includes("membership") ? "clrw" : ""}>Write</Link>
             </li>
           </ul>
-          <Link to="/signup" className="sign-in">
+          <Link to="/signup" className={"sign-in "+(location.pathname.includes("membership") ? "clrw" : "")}>
             Sign In
           </Link>
-          <Link to="/signup" className="navbar-button" id="navButton">
+          <Link to="/signup" className={"navbar-button "+(colorChange ? "greenButton" : "")} id="navButton">
             Get started
           </Link>
         </div>
